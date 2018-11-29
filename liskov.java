085c3de5e0897bee3b53220fbb3cce0c6c25f8b8
Annuci4 <36335608+Annuci4@users.noreply.github.com>
2018-11-29 16:20:43 +0100

    Update liskov.java

--- liskov.java
+++ liskov.java
@@ -1,1 +1,45 @@
+// ez a T az LSP-ben
+public class Madar
+{
+	 public void repul()
+	 {
+	 }
+}
+
+// ez a két osztály alkotja a "P programot" az LPS-ben
+public class Program
+{
+	 public final void fgv(Madar madar)
+	 {
+		  madar.repul();
+	 }
+}
+
+// itt jönnek az LSP-s S osztályok
+public class Sas extends Madar
+{
+}
+
+public class Pingvin extends Madar // ezt úgy is lehet/kell olvasni, hogy a pingvin tud repülni
+{
+}
+
+package <missing>;
+
+public class GlobalMembers
+{
+	public static void Main(String[] args)
+	{
+		 Program program = new Program();
+		 Madar madar = new Madar();
+		 program.fgv(madar);
+
+		 Sas sas = new Sas();
+		 program.fgv(sas);
+
+		 Pingvin pingvin = new Pingvin();
+		 program.fgv(pingvin); // sérül az LSP, mert a P::fgv röptetné a Pingvint, ami ugye lehetetlen.
+
+	}
+}
 
