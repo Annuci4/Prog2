83c63d1c5c21233511f9dd0f05a41545d266a469
Annuci4 <36335608+Annuci4@users.noreply.github.com>
2018-11-30 15:28:35 +0100

    Create Szulo_gyerek.java

---
+++ Szulo_gyerek.java
@@ -0,0 +1,20 @@
+public class Szulo_gyerek{
+ class Szulo{
+}
+
+  class Gyerek extends Szulo
+	{
+		public void viselkedes()
+		{
+		}
+	}
+	public static void main(String[] args)
+		{
+			Szulo_gyerek sz_gy = new Szulo_gyerek();
+				
+			Szulo szulo = sz_gy.new Gyerek();
+			Szulo.viselkedes();
+			
+	
+		}
+}
